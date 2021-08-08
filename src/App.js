0240784d8c021d7cff53873dd8cfc2d33cbb34ba
Ariel Peralta <arielperalta_69@hotmail.com>
2021-08-08 16:10:16 -0300

    Agregando un NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <NavBar />  
         <p>
           My Online Store - Peralta Aliano Luis Ariel 
         </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +24,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return <nav>
+        <ul>Menú
+            <li>Home</li>
+            <li>Categoria 1</li>
+            <li>Categoria 2</li>
+            </ul> 
+            </nav>
+};
+
+export default NavBar;
